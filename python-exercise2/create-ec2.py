515f70d3e4715051878b2e5cf7823e2bb9144129
Zenghui Huang <huihui127127@gmail.com>
2021-09-16 11:23:25 +0100

    update

--- python-exercise2/create-ec2.py
+++ python-exercise2/create-ec2.py
@@ -1,9 +1,24 @@
-import sys
+import argparse,sys
 import boto3
-ec2 = boto3.resource('ec2')
-input_name = sys.argv[1]
 
-instance = ec2.create_instances(
+
+
+
+#This function will be executed when the file is run from command line and not imported. 
+def main():
+    create_ec2_instances()
+   
+
+#This function creates one instance with the name specified by the user.
+#The name of the instance is passed as an argument from command line.
+def create_ec2_instances():
+    ec2 = boto3.resource('ec2')
+    #input_name = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="Insert the name of the instance that you want to create: ")
+    args = parser.parse_args()
+    print(args.name)
+    instance = ec2.create_instances(
     ImageId='ami-00dfe2c7ce89a450b',
     MinCount=1,
     MaxCount=1,
@@ -14,14 +29,20 @@ instance = ec2.create_instances(
             'Tags': [
                 {
                     'Key': 'Name',
-                    'Value': input_name
+                    'Value': args.name
                 },
             ]
         },
     ] 
+    )
+
+if __name__ == "__main__":
+    main()
 
 
-)
+
+
+
 
 
 
